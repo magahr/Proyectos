3d4f5255cebf81ba19b31ce6b048e69b6528f665
Magaly Hernández <magahr123@gmail.com>
2024-06-10 16:38:19 -0400

    Second commit

--- Python/app.py
+++ Python/app.py
@@ -1,1 +1,11 @@
 print("Hola Mundo")
+numero = 10
+numero2 = 10
+numero3 = numero + numero2
+print ("valor de numero ", numero)
+print ("valor de la posicion de memoria de numero ", id(numero))
+print ("valor de numero2 ", numero2)
+print ("valor de la posicion de memoria de numero2 ", id(numero2))
+print ("valor de numero3 ", numero3)
+print ("valor de la posicion de memoria de numero3 ", id(numero3))
+print ("Tipo de datos de la variable numero ", type(numero))
