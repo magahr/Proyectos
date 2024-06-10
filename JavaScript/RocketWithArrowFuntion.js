04d918a716b32ac13b79f01a41b44bb5c2e7aae8
Magaly Hernández <magahr@yahoo.com>
2024-06-09 22:22:51 -0400

    Delete JavaScript directory

--- JavaScript/RocketWithArrowFuntion.js
+++
@@ -1,8 +0,0 @@
-// console.log(falcon9Roket.name)
-// console.log(falconHeavyRocket.name)
-// console.log(falcon9Roket.launchMessage())
-const RocketWithArrowFuntion = (name, message) => {
-    name: name,
-        message; message;
-
-};
