7d96799edc91b44830ecfcaf2a881da1ae66e63c
MagaDev <magahr123@gmail.com>
2024-07-15 22:45:38 -0400

    My First  API

---
+++ Python/Flask-Grupo-8/hello_worl.py
@@ -0,0 +1,11 @@
+from flask import flask
+
+app = flask(__name__)
+
+app.route('/')
+def hello_worl():
+    return 'hello word'
+
+app.route('/aples, methods |'GET','POST')
+def social_oplesk();
+   retur '<h1>hello oplek</h1>'
